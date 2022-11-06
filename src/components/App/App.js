a96331dac6f9682c288e078821f2961f29890ebb
Anastasia Volzhina <happy-heart@bk.ru>
2022-11-06 23:03:39 +0300

    cors

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,37 @@
 import Header from '../header/header';
 import './App.css';
+import React from 'react';
 import Movies from '../Movies/Movies';
 import SavedMovies from '../SavedMovies/SavedMovies'
 import Main from '../Main/Main'
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, useHistory } from 'react-router-dom';
 import Footer from '../Footer/Footer';
 import Register from '../Register/Register';
 import Login from '../Login/Login';
 import NotFound from '../NotFound/NotFound';
 import Profile from '../Profile/Profile';
+import { CurrentUserContext } from '../../contexts/CurrentUserContext'
+import * as auth from '../../utils/auth'
 
 function App() {
+  const [currentUser, setСurrentUser] = React.useState(null);
+  const history = useHistory();
+
+  const onRegister = (data) => {
+    return auth
+      .register(data)
+      .then(() => {
+        history.push('/signin');
+        //setAuthMessage(true);
+      })
+      .catch(()=>{
+        //setAuthMessage(false);
+      })
+  };
+
   return (
     <div className="app">
+    <CurrentUserContext.Provider value={currentUser}>
     <Header
     />
     <Switch>
@@ -26,7 +45,9 @@ function App() {
         <SavedMovies />
       </Route>
       <Route path="/signup">
-        <Register />
+        <Register
+          onRegister={onRegister}
+        />
       </Route>
       <Route path="/signin">
         <Login />
@@ -39,8 +60,7 @@ function App() {
       </Route>
     </Switch>
     <Footer />
-
-    
+    </CurrentUserContext.Provider>
     </div>
   );
 }
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,22 +1,48 @@
 import "./Register.css"
 import { Link } from 'react-router-dom';
 import headerLogo from '../../images/logo/logo.svg';
+import React from 'react';
 
-function Register() {
+function Register({onRegister}) {
+
+    const [registerData, setRegisterData] = React.useState({
+        name: "",
+        email: "",
+        password: "",
+      });
+
+    const [message, setMessage] = React.useState('');
+
+    const handleChange = (e) => {
+        setMessage('');
+        const { name, value } = e.target;
+        setRegisterData({
+          ...registerData,
+          [name]: value,
+        });
+      };
+
+      const handleSubmit = (e) => {    
+        e.preventDefault();
+        onRegister(registerData)
+          .catch(err => setMessage(err.message || 'Что-то пошло не так'));
+      };
+
+
     return (
         <div className="register">
             <Link to="/">
                 <img className="header__logo_register" src={headerLogo} alt="Логотип страницы" />
             </Link>
             <p className="register__welcome">Добро пожаловать!</p>
-            <form className="register__form">
+            <form onSubmit={handleSubmit} className="register__form">
                 <label for ="name" className="register__lable">Имя</label>
-                <input className="register__input" id="name" name="name" type='text' />
+                <input className="register__input" id="name" name="name" type='text' value={registerData.name} onChange={handleChange} />
                 <label for ="email" className="register__lable">E-mail</label>
-                <input className="register__input" id='email' name="email" type='email'/>
+                <input className="register__input" id='email' name="email" type='email' value={registerData.email} onChange={handleChange}/>
                 <label for ="password" className="register__lable">Пароль</label>
-                <input className="register__input" id="password" name="password" type="password" />
-                <button type="submit" className="link register__button">Зарегистрироваться</button>
+                <input className="register__input" id="password" name="password" type="password" value={registerData.password} onChange={handleChange} />
+                <button type="submit" className="link register__button" onSubmit={handleSubmit}>Зарегистрироваться</button>
           </form>
             <p className="register__signin">Уже зарегистрированы?<Link to="/signin" className="link register__login-link"> Войти</Link></p>
         </div>
